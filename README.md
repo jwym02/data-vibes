56ded661306eba2954bfa9a8a412f973050d7209
Jovan <jovanwang2002@gmail.com>
2025-01-22 22:06:47 +0800

    set up setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # data-vibes
+
 SMUBIA Dathathon Data Vibes Only project
+
+<!-- ignore -->
+<!-- pip install pandas matplotlib networkx numpy scikit-learn nltkpip install pandas matplotlib networkx numpy scikit-learn nltk -->
---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib as plt
+import networkx as nx
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
